a8a703d00cfc8f8f15cbc025d132a37971dd6827
faroukq1 <ouledmeriemfarouk1@gmail.com>
2025-07-31 15:19:34 +0100

    feat: add skeleton loader for lesson video and finalize project

--- app/dashboard/[slug]/[lessonId]/page.tsx
+++ app/dashboard/[slug]/[lessonId]/page.tsx
@@ -1,10 +1,22 @@
 import { getLessonContent } from '@/app/data/course/get-lesson-content';
 import CourseContent from './_component/CourseContent';
+import { Suspense } from 'react';
+import LessonSkelaton from './_component/LessonSkelaton';
 
 type Params = Promise<{ lessonId: string }>;
 
 export default async function LessonPageRoute({ params }: { params: Params }) {
   const { lessonId } = await params;
+  const data = await getLessonContent(lessonId);
+
+  return (
+    <Suspense fallback={<LessonSkelaton />}>
+      <LessonContentLoader lessonId={lessonId} />
+    </Suspense>
+  );
+}
+
+async function LessonContentLoader({ lessonId }: { lessonId: string }) {
   const data = await getLessonContent(lessonId);
   return <CourseContent data={data} />;
 }
--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,7 +1,21 @@
-export default function CourseSlugRoute() {
+import { getCourseSidebarData } from '@/app/data/course/get-course-sidebar-data';
+import { redirect } from 'next/navigation';
+
+interface iAppProps {
+  params: Promise<{ slug: string }>;
+}
+export default async function CourseSlugRoute({ params }: iAppProps) {
+  const { slug } = await params;
+  const course = await getCourseSidebarData(slug);
+  const firstChapter = course.course.chapter[0];
+  const firstLesson = firstChapter.lesson[0];
+  if (firstLesson) redirect(`/dashboard/${slug}/${firstLesson.id}`);
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="flex flex-col h-screen items-center justify-center text-center">
+      <h2 className="text-4xl font-bold mb-2">No lesson available</h2>
+      <p className="text-xl text-muted-foreground">
+        This course does not have any lessons yet!
+      </p>
     </div>
   );
 }
